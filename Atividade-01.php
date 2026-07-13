b1639a83e5667418c8bb8d510b3c2ed4d5276e6b
Caua <cauasantiagofreitas23241@gmail.com>
2026-07-13 19:56:44 -0300

    Atitvidade01

---
+++ Atividade-01.php
@@ -0,0 +1,43 @@
+<?php
+/*Atividade 1 - Pagamento de funcionário
+Uma empresa deseja calcular o salário de um funcionário. Armazene e calcule
+● o nome do funcionário.
+● o valor da hora trabalhada.
+● a quantidade de horas trabalhadas.
+● o salário bruto.
+● exiba o nome, o salário bruto, o desconto e o salário final.
+● Se o salário bruto for maior que R$ 2.500,00, desconte R$ 200,00. */
+
+// Entradas
+$nomeFuncionario = "Michelle";
+$valorHoraTrabalhadas = 2500.00;
+$qtdHorasTrabalhadas = 200;
+$salarioBruto = $valorHoraTrabalhadas * $qtdHorasTrabalhadas;
+$desconto = 200.00;
+$salarioLiquido = $salarioBruto; // inicializar
+
+if ($salarioBruto > 2500.00) {
+    $salarioLiquido = $salarioBruto - $desconto;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web</title>
+</head>
+
+<body>
+    <h3>Atividade 1</h3>
+    <p> <?php echo ("Nome do funcionário: " . $nomeFuncionario); ?></p>
+    <p> <?php echo ("Valor da hora trabalhadas: " . $valorHoraTrabalhadas); ?></p>
+    <p> <?php echo ("Quantidade de horas trabalhadas: " . $qtdHorasTrabalhadas); ?></p>
+    <p> <?php echo ("Desconto: " . $desconto); ?></p>
+    <p> <?php echo ("Salário Líquido: " . $salarioLiquido); ?></p>
+</body>
+
+</html>
