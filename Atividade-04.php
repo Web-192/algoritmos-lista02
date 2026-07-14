f459f7de022f9af325f74f3df602cfa638a91d40
Caua <cauasantiagofreitas23241@gmail.com>
2026-07-13 21:51:19 -0300

    atividade 4

---
+++ Atividade-04.php
@@ -0,0 +1,35 @@
+<?php
+/*Atividade 4 - Média de um aluno
+Um professor precisa calcular a média de um aluno. Armazene e calcule:
+● Armazene o nome do aluno.
+● Armazene três notas.
+● Calcule a soma das notas.
+● Calcule a média.
+● Exiba o nome, as notas e a média.
+● Se a média for maior ou igual a 60, exiba: Aluno aprovado.*/
+
+// Entradas
+$nomeAluno = "Caua";
+$nota1 = 24;
+$nota2 = 21;
+$nota3 = 18;
+
+// Processamento
+$totalSoma = $nota1 + $nota2 + $nota3;
+$media = $totalSoma / 4
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web</title>
+</head>
+<body>
+    <h3>Atividade 4</h3>
+    <p>
+
+    </p>
+</body>
+</html>
